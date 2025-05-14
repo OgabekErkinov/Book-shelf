a91e2448a9a9a10edb790f55cc92291442eeb4f8
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-05-14 10:49:26 +0500

    axios had been installed

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,10 +1,28 @@
 import { Box, Button, Input, Stack, Typography } from "@mui/material"
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const Auth = () => {
     const [signIn, setSignIn] = useState<boolean>(true)
+    const signUpUser = async () => {
+  try {
+    const response = await axios.post('https://no23.lavina.tech/signup', {
+      name: "Og'abek",
+      email: "ogabek2025@example.com",
+      key: "ogabekKey123",
+      secret: "ogabekSecret456"
+    });
+
+    console.log("Signup success:", response.data);
+  } catch (error) {
+    console.error("Signup error:", error);
+  }
+};
+useEffect(() => {
+    signUpUser()
+},[])
   return (
-    <Box height='520px' width='430px'  borderRadius='12px'
+    <Box height='auto' width='430px'  borderRadius='12px'
          bgcolor='#FEFEFE' boxShadow='0 4px 32px 0 #3333330A'>
             <Stack height='100%' width='100%' padding='48px 28px' display='flex' gap='32px' >
                 <Typography fontWeight='700' 
@@ -12,23 +30,23 @@ const Auth = () => {
                             lineHeight='100%'
                             textAlign='center'
                             fontFamily='Mulish'>
-                    Sign Up</Typography>
+                    { signIn ? 'Sign In' : 'Sign Up' }</Typography>
 
-                <Stack height='240px' width='380px' gap='16px'>
-                    <Stack gap='4px'>
+                <Stack height='auto' width='380px' gap='12px'>
+                    <Stack>
                         <Typography>username</Typography>
                         <Input disableUnderline placeholder="Enter your username"
-                               sx={{padding : '12px 14px'}}/>
+                               sx={{padding : '8px 12px'}}/>
                     </Stack>
-                    <Stack gap='4px'>
+                    <Stack>
                         <Typography>password</Typography>
                         <Input disableUnderline placeholder="Enter your password"
-                               sx={{padding : '12px 14px'}}/>
+                               sx={{padding : '8px 12px'}}/>
                     </Stack>
-                    <Stack gap='4px'>
+                    <Stack display={signIn ? 'none' : 'flex'}>
                         <Typography>confirm password</Typography>
                         <Input disableUnderline placeholder="Enter your confirm password"
-                                sx={{padding : '12px 14px'}}/>
+                                sx={{padding : '8px 12px'}}/>
                     </Stack>
 
                 </Stack>
@@ -37,8 +55,16 @@ const Auth = () => {
                     <Button disableTouchRipple 
                     sx={{height : '40px', width : '380px', padding : '10px 24px', 
                          bgcolor : '#6200EE', color : '#fff'}}>
-                        Submit</Button>
-                    <Typography fontSize='12px' textAlign='center'>Already signed up Go to sign in</Typography>
+                        { signIn ? 'Enter' : 'Submit' }</Button>
+                    <Box display='flex' alignItems='center' justifyContent='center'>
+                       <Typography fontSize='12px' textAlign='center'>Already signed up</Typography>
+                       <Button disableTouchRipple onClick={() => setSignIn(prev => !prev)}>
+                         <Typography fontSize='12px' textTransform='lowercase'>
+                            Go to {signIn ? 'sign up' : 'sign in'}
+                        </Typography>
+                       </Button>
+                    </Box>
+                    
                 </Stack>
 
 
